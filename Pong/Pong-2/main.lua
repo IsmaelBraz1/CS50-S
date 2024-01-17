9dddd778724b392af8eba337459a82cbeafc066f
IsmaelBraz <brazismael1@gmail.com>
2024-01-17 17:42:51 -0300

    pong3

--- Pong/Pong-2/main.lua
+++ Pong/Pong-2/main.lua
@@ -5,11 +5,23 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+--velocidade das pas
+PADDLE_SPEED = 200
 
 function love.load()
     -- use a filtragem do vizinho mais próximo no aumento e na redução para evitar o desfoque do texto
     -- e gráficos; tente remover esta função para ver a diferença!
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    --nova fonte para o score
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+
+    love.graphics.setFont(smallFont)
+
+    --gerador aleatorio baseado no tempo
+    math.randomseed(os.time())
     -- inicializar nossa resolução virtual, que será renderizada dentro de nosso
     -- janela real, não importando suas dimensões; substitui nossa chamada love.window.setMode
     -- do último exemplo
@@ -18,18 +30,77 @@ function love.load()
         resizable = false,
         vsync = true
     })
+    --inicialização das variasveis da pontuaçao dos players
+    player1Score = 0
+    player2Score = 0
+
+    --posicao inicial das pas
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+
+    --velocidade e tamanho da bola
+    ballX = VIRTUAL_WIDTH / 2 - 2
+    ballY = VIRTUAL_HEIGHT / 2 - 2
+
+    --da uma direcao incial aleatorio a bola
+    ballDX = math.random(2) == 1 and 100 or -100
+    ballDY = math.random(-50, 50)
+
+    gameState = "start"
 end
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else
+            gameState = 'start'
+
+            ballX = VIRTUAL_WIDTH / 2 - 2
+            ballY = VIRTUAL_HEIGHT / 2 - 2
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50, 50) * 1.5
+        end
+    end
+end
+
+function love.update(dt)
+    --movimentacao do player1
+    if love.keyboard.isDown('w') then
+        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('s') then
+        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+    end
+
+    --movimentacao do player2
+    if love.keyboard.isDown('up') then
+        player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('down') then
+        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+    end
+
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
     end
 end
 
 function love.draw()
     --comeca a renderizar em resolução virtual
     push:apply('start')
-    love.graphics.printf("Hello Pong", 0,  VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+    love.graphics.setFont(smallFont)
+    love.graphics.printf("Hello Pong", 0, 20, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 50, VIRTUAL_HEIGHT / 3)
+
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
     push:apply('end')
-   
 end
---
+++ Pong/Pong-3/main.lua
@@ -0,0 +1,106 @@
+push = require 'push'
+
+Class = require 'class'
+
+require 'Paddle'
+require 'Ball'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+--velocidade das pas
+PADDLE_SPEED = 200
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle('Pong')
+    math.randomseed(os.time())
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+    love.graphics.setFont(smallFont)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+    gameState = "start"
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else
+            gameState = 'start'
+
+            ball:reset()
+        end
+    end
+end
+
+function love.update(dt)
+    --movimentacao do player1
+    if love.keyboard.isDown('w') then
+        player1.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('s') then
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
+    end
+
+    --movimentacao do player2
+    if love.keyboard.isDown('up') then
+        player2.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('down') then
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
+    end
+
+    if gameState == 'play' then
+        ball:update(dt)
+    end
+
+    player1:update(dt)
+    player2:update(dt)
+end
+
+function love.draw()
+    --comeca a renderizar em resolução virtual
+    push:apply('start')
+
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+    love.graphics.setFont(smallFont)
+    love.graphics.printf("Hello Pong", 0, 20, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,  VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
+    player1:render()
+    player2:render()
+    ball:render()
+    displayFPS()
+    push:apply('end')
+end
+
+function displayFPS()
+    -- simple FPS display across all states
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255/255, 0, 255/255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()),VIRTUAL_WIDTH / 2 - 12, 5)
+end
