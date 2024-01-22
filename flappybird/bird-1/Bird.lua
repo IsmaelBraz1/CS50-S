9ce3bc4519aa3c6c50a3c90e003956483417924b
IsmaelBraz <brazismael1@gmail.com>
2024-01-22 11:09:17 -0300

    bird1

---
+++ flappybird/bird-1/Bird.lua
@@ -0,0 +1,28 @@
+Bird = Class{}
+
+GRAVITY = 20
+
+function Bird:init()
+    self.image = love.graphics.newImage('bird.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    --posicionamento da imagem
+    self.x = VIRTUAL_WIDTH/2 - (self.width/2)
+    self.y = VIRTUAL_HEIGHT/2 - (self.height/2)
+
+    self.dy = 0
+end
+
+function Bird:update(dt)
+    self.dy = self.dy + GRAVITY*dt
+    self.y = self.y + self.dy
+
+    if self.y > VIRTUAL_HEIGHT - self.height - 16 then
+        self.y = VIRTUAL_HEIGHT - self.height -16
+    end
+end
+
+function Bird:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end
--- flappybird/bird-1/main.lua
+++ flappybird/bird-1/main.lua
@@ -1,14 +1,28 @@
 push = require 'bird-0.push'
 
+Class = require 'class'
+require 'Bird'
+--tamanho real da janela
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+--tamanho virtual da janela
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
 local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
 
+--Velocidade da rolagem
+BACKGROUND_SCROLL_SPEED = 30
+GROUND_SCROLL_SPEED = 60
+
+--ponto de loop das imgs
+BACKGROUND_LOOPING_POINT = 413
+
+local bird = Bird()
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     love.window.setTitle('Flappy bird')
@@ -29,12 +43,23 @@ function love.keypressed(key)
         love.event.exit()
     end
 end
+
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED*dt)
+        %BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED*dt)
+        %BACKGROUND_LOOPING_POINT
+
+    bird:update(dt)
+end
  
 function love.draw()
     push:start()
     
-    love.graphics.draw(background,0,0)
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT-16)
+    love.graphics.draw(background,-backgroundScroll,0)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT-16)
 
+    bird:render()
     push:finish()
 end
