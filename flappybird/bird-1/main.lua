e029f84ea482759ebb71abef8aafae17b61772ab
IsmaelBraz <brazismael1@gmail.com>
2024-01-23 17:05:21 -0300

    pipepair updade

--- flappybird/bird-1/main.lua
+++ flappybird/bird-1/main.lua
@@ -1,8 +1,9 @@
-push = require 'bird-0.push'
+push = require 'push'
 
 Class = require 'class'
 require 'Bird'
 require 'Pipe'
+require 'PipePair'
 --tamanho real da janela
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -24,29 +25,29 @@ GROUND_SCROLL_SPEED = 60
 BACKGROUND_LOOPING_POINT = 413
 
 local bird = Bird()
-local pipes = {}
+local pipePairs = {}
 local spawnTimer = 0
+local scrolling = true
+local lastY = -PIPE_HEIGHT + math.random(80) + 20
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     love.window.setTitle('Flappy bird')
 
-    push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
         resizable = true
     })
 
     love.keyboard.keyspressed = {}
-
 end
 
-function love.resize(w,h)
-    push:resize(w,h)
+function love.resize(w, h)
+    push:resize(w, h)
 end
 
 function love.keypressed(key)
-
     love.keyboard.keyspressed[key] = true
 
     if key == 'escape' then
@@ -63,41 +64,57 @@ function love.keyboard.wasPressed(key)
 end
 
 function love.update(dt)
-    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED*dt)
-        %BACKGROUND_LOOPING_POINT
+    if scrolling then
+        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+            % BACKGROUND_LOOPING_POINT
 
-    groundScroll = (groundScroll + GROUND_SCROLL_SPEED*dt)
-        %BACKGROUND_LOOPING_POINT
+        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+            % BACKGROUND_LOOPING_POINT
 
-    spawnTimer = spawnTimer + dt
-       
-    if spawnTimer > 2 then
-            table.insert(pipes, Pipe())
+        spawnTimer = spawnTimer + dt
+
+        if spawnTimer > 2 then
+            local y = math.max(-PIPE_HEIGHT + 10,
+                math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+            lastY = y
+
+            table.insert(pipePairs, PipePair(y))
             spawnTimer = 0
-    end
-
-    bird:update(dt)
-
-    for k, pipe in pairs(pipes) do
-        pipe:update(dt)
-
-        if pipe.x < -pipe.width then
-            table.remove(pipes, k)
         end
-    end
 
-    love.keyboard.keyspressed = {}
+        bird:update(dt)
+
+        for k, pair in pairs(pipePairs) do
+            pair:update(dt)
+
+            for l, pipe in pairs(pair.pipes) do
+                if bird:collides(pipe) then
+                    -- pause the game to show collision
+                    scrolling = false
+                end
+            end
+        end
+
+        for k, pair in pairs(pipePairs) do
+            if pair.remove then
+                table.remove(pipePairs, k)
+            end
+        end
+
+        love.keyboard.keyspressed = {}
+    end
 end
- 
+
 function love.draw()
     push:start()
-    
-    love.graphics.draw(background,-backgroundScroll,0)
 
-    for k, pipe in pairs(pipes) do
-        pipe:render()
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+    for k, pair in pairs(pipePairs) do
+        pair:render()
     end
-    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT-16)
+
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
     bird:render()
     push:finish()
